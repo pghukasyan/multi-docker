941a29cdd93257a442b3abe280d37c2983a57ca4
Paul Ghukasyan <paul.ghukasyan@gmail.com>
2020-05-02 09:38:07 +0300

    Starting k8s

--- worker/index.js
+++ worker/index.js
@@ -10,9 +10,7 @@ const sub = redisClient.duplicate();
 
 function fib(index) {
   if (index < 2) return 1;
-  console.log(index);
   return fib(index - 1) + fib(index - 2);
-
 }
 
 sub.on('message', (channel, message) => {
